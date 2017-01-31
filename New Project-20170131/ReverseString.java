11b91434415ee18a04462bd5a9c0b0a652c9a568
fdblender <fdblender@users.noreply.github.com>
2017-01-30 21:24:56 -0500

    Created by Coding Ground Client

---
+++ New Project-20170131/ReverseString.java
@@ -0,0 +1,27 @@
+public class ReverseString {
+    
+    // this method uses the StringBuffer reverse method
+    public static String reverseStringBuffer(String s) {
+        StringBuffer rs = new StringBuffer();
+        rs.append(s);
+        rs.reverse();
+        return rs.toString();
+    }
+    
+    // this method reverses a string one character at a time
+    public static String reverse(String s) {
+        
+        StringBuffer rs = new StringBuffer();
+        
+        for (int i = s.length()-1; i >=0; i--) {
+            rs.append(s.charAt(i));
+        }
+        return rs.toString();
+    }
+    
+    public static void main(String[] args) {
+        String s = "gfedbca";
+        System.out.println(reverse(s));
+        System.out.println(reverseStringBuffer(s));
+    }
+}
